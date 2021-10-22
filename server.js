87f4decae08c61e16231b99e62cc5848adfb1cfd
Ricardo Pineda <rikaldero@hotmail.com>
2021-10-21 23:48:39 -0500

    Configuración de peticiones CRUD para el módulo productos

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 
 //import express manera actual
 import Express from "express";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
+import Cors from "cors";
 
 const stringConexion =
   "mongodb+srv://riky:ecotextil@ecotextil.upg8z.mongodb.net/test?authSource=admin&replicaSet=atlas-udecoi-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true";
@@ -15,14 +16,15 @@ const client = new MongoClient(stringConexion, {
   useUnifiedTopology: true,
 });
 
-let conexion;
+let baseDeDatos;
 
 const app = Express();
 app.use(Express.json());
+app.use(Cors());
 
 app.get("/productos", (req, res) => {
   console.log("Alguien hizo get en la ruta /productos");
-  conexion
+  baseDeDatos
     .collection("productos")
     .find({})
     .limit(50)
@@ -48,7 +50,7 @@ app.post("/productos/nuevo", (req, res) => {
       Object.keys(datosProducto).includes("estado")
     ) {
       //implementar código para crear productos
-      conexion
+      baseDeDatos
         .collection("productos")
         .insertOne(datosProducto, (err, result) => {
           if (err) {
@@ -67,6 +69,47 @@ app.post("/productos/nuevo", (req, res) => {
     res.sendStatus(500);
   }
 });
+//editar productos
+app.patch("/productos/editar", (req, res) => {
+  const edicion = req.body;
+  console.log(edicion);
+  //filtro para realizar actualizaciones
+  const filtroProducto = { _id: new ObjectId(edicion.id) };
+  delete edicion.id;
+  const operacion = {
+    $set: edicion,
+  };
+  baseDeDatos
+    .collection("productos")
+    .findOneAndUpdate(
+      filtroProducto,
+      operacion,
+      { upsert: true },
+      (err, result) => {
+        if (err) {
+          console.error("error actualizando el producto", err);
+          res.sendStatus(500);
+        } else {
+          console.log("Actualizado con éxito");
+          res.sendStatus(200);
+        }
+      }
+    );
+});
+
+app.delete("/productos/eliminar", (req, res) => {
+  const filtroProducto = { _id: new ObjectId(req.body.id) };
+  baseDeDatos
+    .collection("productos")
+    .deleteOne(filtroProducto, (err, result) => {
+      if (err) {
+        console.error(err);
+        res.sendStatus(500);
+      } else {
+        res.sendStatus(200);
+      }
+    });
+});
 
 /*const dbName = "ecotextil";
 
@@ -81,12 +124,12 @@ async function main() {
 const main = () => {
   client.connect((err, db) => {
     if (err) {
-      console.error('Error conectando a la base de datos');
+      console.error("Error conectando a la base de datos");
     }
-    conexion = client.db('EcoTextil');
-    console.log('conexión exitosa');
+    baseDeDatos = client.db("EcoTextil");
+    console.log("conexión exitosa");
     return app.listen(5000, () => {
-      console.log('Escuchando puerto 5000'); 
+      console.log("Escuchando puerto 5000");
     });
   });
 };
